2525726342667f54c82e977cfc0b738fd0f2f4a1
elandrusenki <YOUR@EMAIL.COM>
2021-07-12 13:47:11 -0700

    finished

--- Week2/js/init.js
+++ Week2/js/init.js
@@ -1,15 +1,34 @@
-const myMap = L.map('mapArea').setView([34.0709, -118.444], 5);
+const myMap = L.map('mapArea').setView([34.0709, -118.444], 7);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// adding markers
-let work = L.marker([34.0709, -118.444]).addTo(myMap)
-		.bindPopup('Where I work on campus')
 
-let home = L.marker([34.7409, -122.484]).addTo(myMap)
-		.bindPopup('Second point')
+// create a function to add markers
+function addMarker(lat,lng,title,message,zoom){
+    console.log(message)
+    L.marker([lat,lng]).addTo(myMap).bindPopup(`<h2>${title}</h2>`)
+    createButtons(lat,lng,title,zoom); // new line!!!
+    return message
+}
 
-let random = L.marker([39.7409, -122.484]).addTo(myMap)
-		.bindPopup('Third Point')
+// create a function to add buttons with a fly to command
+function createButtons(lat,lng,title,zoom){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('mouseover', function(){
+        myMap.flyTo([lat,lng],zoom); 
+    })
+    document.body.appendChild(newButton); //this adds the button to our page.
+} 
+
+// use our marker functions
+addMarker(49.895, -97.138,'Grandma', 'Canada')
+addMarker(41.878,-87.629,'Mommy','Chicago')
+addMarker(34.697,-118.144,'Brother','Lancaster')
+addMarker(33.835,-118.340,'Me (Liz)','Torrance')
+
