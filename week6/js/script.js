fe16b9673d5fee158f4e261f4c15e91b629945fc
elandrusenki <YOUR@EMAIL.COM>
2021-07-26 22:00:05 -0700

    updates

---
+++ week6/js/script.js
@@ -0,0 +1,52 @@
+const myMap = L.map('mapArea').setView([34.0709, -118.444], 5);
+
+const url = "https://spreadsheets.google.com/feeds/list/1OQl-LHXk1y1bA33bpsDj1bdu71Nqkny_5VOdNqPWciE/of9zpxw/public/values?alt=json"
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+                // console.log(data)
+                formatData(data)
+        }
+)
+
+function addMarker(data){
+    L.marker([data.lat,data.lng]).addTo(myMap).bindPopup(`<h2>${data.timestamp}</h2>`)
+    createButtons(data.lat,data.lng,data.location)
+    return data.timestamp
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button");
+    newButton.id = "button"+title;
+    newButton.innerHTML = title;
+    newButton.setAttribute("lat",lat); 
+    newButton.setAttribute("lng",lng);
+    newButton.addEventListener('click', function(){
+        myMap.flyTo([lat,lng]);
+    })
+    const spaceForButtons = document.getElementById('contents')
+    spaceForButtons.appendChild(newButton);
+}
+
+function formatData(theData){
+        const formattedData = []
+        const rows = theData.feed.entry
+        for(const row of rows) {
+          const formattedRow = {}
+          for(const key in row) {
+            if(key.startsWith("gsx$")) {
+                  formattedRow[key.replace("gsx$", "")] = row[key].$t
+            }
+          }
+          formattedData.push(formattedRow)
+        }
+        console.log(formattedData)
+        formattedData.forEach(addMarker)        
+}
