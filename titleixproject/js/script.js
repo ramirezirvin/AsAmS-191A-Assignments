d09cb197dc9eb3a8956481e23967184c0eb4fee2
ramirezirvin <irvinramirez96@gmail.com>
2021-07-29 19:01:59 -0700

    Co-authored-by: eandrusenki <eandrusenki@users.noreply.github.com> Co-authored-by: albertkun <albertkun@users.noreply.github.com>

--- titleixproject/js/script.js
+++ titleixproject/js/script.js
@@ -2,7 +2,7 @@ const myMap = L.map('mapArea').setView([34.0709, -118.444], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+}).addTo(myMap); 
 
 function addMarker(data){
     // console.log(data)
@@ -38,5 +38,4 @@ function processData(theData){
     }
     console.log(formattedData)
     formattedData.forEach(addMarker)   
-   
-}
+  }
